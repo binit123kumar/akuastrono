9dd86fabb95fb3c52d90da319a84fcdf72bba53d
Tanu Priya <tanupriyaa15@gmail.com>
2026-05-26 12:59:44 +0530

    fix: resolve blank page by adding root div and fixing routes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import "./App.css";
-import About from "./components/About";
-import Contact from "./pages/Contact";
-import Home from "./components/Home";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-import HeroSection from './components/HeroSection'
+import "./App.css";
+
+import Home from "./components/Home";
+import About from "./components/About";
+import HeroSection from "./components/HeroSection";
+import HomeButton from "./components/HomeButton";
+import Footer from "./components/Footer";
+
+import Contact from "./pages/Contact";
 import Faculty from "./pages/Faculty";
 import AimAndObjective from "./pages/AimAndObjective";
 import Event from "./pages/Event";
-import HomeButton from "./components/HomeButton";
-import Footer from './components/Footer'
 
 function App() {
   return (
     <>
-      <HeroSection/>
+      <HeroSection />
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/Faculty" element={<Faculty/>} />
-        <Route path="/AimAndObjective" element={<AimAndObjective/>} />
-        <Route path="/Event" element={<Event/>} />
+        <Route path="/Faculty" element={<Faculty />} />
+        <Route path="/AimAndObjective" element={<AimAndObjective />} />
+        <Route path="/Event" element={<Event />} />
       </Routes>
-      <HomeButton/>
-      <Footer/>
+
+      <HomeButton />
+      <Footer />
     </>
   );
 }
