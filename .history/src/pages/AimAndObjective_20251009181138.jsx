9095780d520a8026d3ff8cc274b24f6e43cdf1d4
Tanu Priya <tanupriyaa15@gmail.com>
2026-05-26 12:33:13 +0530

    chore: project structured properly for vercel

---
+++ .history/src/pages/AimAndObjective_20251009181138.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import '../Styles/Aim.css'
+function AimAndObjective() {
+  return (
+    <div className="AimdBox">
+      <h1 style={{textAlign: "center"}}>Aim And Objective</h1>
+      <h3 className="AimContent">
+        
+  Aim and objectives 
+
+To make knowledge of space science and Astronomy accessible to all.
+Our objectives are to act for the advancement of knowledge through research in space science and technology.
+
+Vision
+
+The overarching vision for astronomy in India involves a holistic approach to advancing scientific research, developing national infrastructure, fostering talent through education and outreach, and promoting global collaboration in the field. Key objectives include answering fundamental questions about the universe, contributing to international mega science projects, and making astronomy accessible to all Indians through improved curricula and public engagement initiatives. 
+
+Mission
+Fostering Scientific Temperament:
+
+Many institutions aim to cultivate a scientific mindset in students from a young age through hands-on learning experiences in astronomy and space science.
+Organizations like Space Technology and Education Pvt.ltd, also known as SPACE India, focus on experiential and hands-on learning to make astronomy accessible and engaging for students, according to SPACE India. 
+Promoting Research and Education:
+
+The Inter-University Centre for Astronomy and Astrophysics (IUCAA) is dedicated to promoting research and providing advanced training in astronomy and astrophysics. 
+Institutions are working to establish astronomy and space labs (Khagolshala) in schools to facilitate practical learning. 
+Developing Skilled Professionals:
+
+A core mission is to train future scientists, explorers, and entrepreneurs in the fields of astronomy and space science. 
+Institutions like India Space Academy offer programs to prepare students for careers in the space industry and related fields. 
+Contributing to Space Exploration and Technology:
+
+Many institutions participate in research related to space exploration, including studying celestial objects and phenomena. 
+The AstroSat mission, led by ISRO, aims to understand high-energy processes in the universe. 
+There is also a focus on developing space technology and participating in international collaborations like the NISAR mission. 
+Public Outreach and Education:
+
+Several organizations are involved in public outreach programs to raise awareness about astronomy and space science. 
+This includes organizing events, workshops, and educational programs to engage the wider community.
+      </h3>
+    </div>
+  );
+}
+
+export default AimAndObjective;
---
+++ .history/src/pages/Faculty_20251009184530.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "../Styles/Faculty.css";
+import Staff from "../Assests/staff1.jpeg";
+import Staff2 from'../Assests/Staff3.jpeg'
+import Staff3 from "../Assests/Staff2.jpeg";
+function Faculty() {
+  return (
+    <>
+      <div className="Faculty-box">
+        <h1>Faculty</h1>
+        <img src={Staff} alt="Staff" />
+        <h3>Dr. Rajmani Prasad Sinha</h3>
+        <h3> M.Sc. (Pat.), M.Sc. (Sheffield, U. K.), Ph.D. (Pat)</h3>
+        <h3>Consultant, School of Astronomy, Aryabhatta Knowledge University, Patna</h3>
+        <h3>Chairman – 2011, Bihar School Examination Board, Patna</h3>
+        <h3>Retired – 2009, From Patna University Service as Head of Physics Department</h3>
+        <h3>Vice-Chancellor – 2003, L.N. Mithila University, Darbhanga.</h3>
+      </div>
+      
+      <div className="Faculty-box">
+        <h1>Staff</h1>
+        <img src={Staff3} alt="Staff" />
+        <h3>Lalan Kumar Mandal</h3>
+        <h3>Assistant</h3>
+        <h3>School of Astronomy </h3>
+        <h3> Email - mandallallan@gmail.com</h3>
+        <h3> CONTACT NO-7903256476</h3>
+      </div>
+      <div className="Faculty-box">
+        <img src={Staff2} alt="Staff" />
+        <h3>Er. MAHTAB ALAM</h3>
+        <h3>CONTACT NO.-9386235343</h3>
+        <h3>School of Astronomy </h3>
+      </div>
+    </>
+  );
+}
+
+export default Faculty;
